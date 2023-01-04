59aa9ecc32b4ea9cf01bf7cf757ed12abad667ea
Leanne <lkeeley@pace.edu>
2023-01-04 13:13:50 -0500

    tweaking visuals

--- checkout.php
+++ checkout.php
@@ -6,13 +6,12 @@
     <title>Rainy Bookstore - Checkout</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="assets/styles.css">
-    <link rel="stylesheet" type="text/css" href="assets/modal.css">
 </head>
 <body>
 <header>
     <h1>Checkout</h1>
 </header>
-<main>
+<main id="checkoutpg">
 <?php
 /* 
     PRICES
@@ -21,16 +20,29 @@
     html: 35
     dictionary: 55
 */
+// create book array with the titles as keys and prices as the values
+$book_arr = array(
+    "Learning PHP, MySQL & JavaScript" => 60,
+    "Learning Python" => 49,
+    "HTML & CSS 9th Ed" => 35,
+    "American Heritage Dictionary" => 55,
+);
+
+// retrieve the cart items and total price passed from the shop page
 $cart = explode('|', htmlentities($_GET['cart']));
 $price = htmlentities($_GET['total']);
-echo "<p>Cart: </p><ol>";
+
+//print the information in table-like divs
+echo "<h2>Cart</h2><div class=\"table\"><div class=\"row header\"><div class=\"cell\">Title</div><div class=\"cell\">Price</div></div>";
 for ($i = 0; $i < count($cart); $i++) {
     $title = $cart[$i];
-    echo "<li>$title</li>";
+    echo "<div class=\"row\"><div class=\"cell\">$title</div><div class=\"cell\">$</div></div>";
 }
-echo "</ol><p>Price: $$price</p>";
+// calculate tax and echo it in a new table row 
 
-// you code here
+echo "<div class=\"row summary\"><div class=\"cell\">Total Price:</div><div class=\"cell\">$$price</div></div>";
+
+// 
 
 ?>
 </main>
